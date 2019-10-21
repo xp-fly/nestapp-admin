fe93384f4ae97b355624d89eb04dbd558740e60f
xp-fly <793832050@qq.com>
2019-10-21 23:19:23 +0800

    [feature] add configure module

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './controller/user.controller';
 import { UserService } from './service/user.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from 'nestjs-configure';
+import { resolve } from 'path';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot()
+        // 加载配置
+        ConfigModule.load(resolve(__dirname, 'bootstrap.yml'), 'admin'),
     ],
     controllers: [
         UserController
