0efdd713befbe6070d3e3c00c792fcd176344672
1053910251 <1053910251@qq.com>
2019-10-23 15:23:43 +0800

    [feature] add database module

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import { UserService } from './service/user.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from 'nestjs-configure';
 import { resolve } from 'path';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
     imports: [
         // 加载配置
         ConfigModule.load(resolve(__dirname, 'bootstrap.yml'), 'admin'),
+        // 数据库模块
+        DatabaseModule.init('admin'),
     ],
     controllers: [
         UserController
